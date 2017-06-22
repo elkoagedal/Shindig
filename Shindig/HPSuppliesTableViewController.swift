02f008fb1f7af720764b17293fe9ca6c7e40be3a
Ryann <rjc6ez@virginia.edu>
2017-06-22 13:38:40 +0200

    some stuff

--- Shindig/HPSuppliesTableViewController.swift
+++ Shindig/HPSuppliesTableViewController.swift
@@ -164,15 +164,27 @@ class HPSuppliesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if (segue.identifier == "supplyDetail") {
+            
+            let sup = segue.destination as! SignUpForSupplyViewController
+            
+            let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPath(for: cell)
+            
+            sup.key = key
+            sup.supplyName = suppliesOpen[(indexPath?.row)!]
+            
+            
+        }
     }
-    */
+    
 
 }
 
